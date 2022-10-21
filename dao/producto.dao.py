5e2760b5a865fd254e0d6568a4f7cec28e83e0ab
EduHerr <eduherrald@gmail.com>
2022-10-21 13:16:30 -0500

    terminando crud

--- dao/producto.dao.py
+++ dao/producto.dao.py
@@ -7,12 +7,75 @@ class DAOProducto:
     ##
     def save():
         ## dynamic data
-        # Producto(product.sku, product.nombre, product.descripcion, product.precio, product.stock)
-        produc = Producto('SKU-03', 'LECHE', 'PRODUCTO LACTEO', 10, 100)
+        # produc = Producto(product.sku, product.nombre, product.descripcion, product.precio, product.stock)
+        produc = Producto("SKU-05", 'Example1', 'This is an example1', 1000, 2222)
         
         ##Validamos la existencia del archivo => Obtenemos el nombre del documento
         fsDocName = Documento.validate('../datasource/', 'productos.json')
-        Documento.write(fsDocName, produc)
+        response = Documento.write(fsDocName, produc)
 
-DAOProducto.save()
+        if response == True:
+            return 'Producto guardado con exito'
+    
+    def read():
+        ##
+        route = '../datasource/productos.json'
+        _productos = Documento.read(route)
+        return _productos
+    
+    def drop(_id):
+        ##
+        route = '../datasource/productos.json'
+        _productos = Documento.read(route)
+
+        ##Search
+        i = 0
+        deleted = False
+        for Producto in _productos:
+            ##Buscar el objeto
+            if Producto['id'] == _id:
+                deleted = True
+            else:
+                i += 1
+
+        ##Validar si NO SE ENCONTRO EL _id
+        if deleted == False:
+            print('El _id que se esta intentando buscar, NO EXISTE!!')
+
+        ##Delet object from collection
+        del _productos[i]
+
+        print('New data:\n', _productos)
+
+        ##Sobre-escribir la data en static_file
+        Documento.write(route, _productos, True)
+        
+        ##
+        return 'Producto borrado con exito'
+    
+    def consult(_id):
+        ##
+        route = '../datasource/productos.json'
+        _productos = Documento.read(route)
+
+        for Producto in _productos:
+            ##Buscar el objeto
+            if Producto['id'] == _id:
+                return Producto
+    
+    def update(data):
+        route = '../datasource/productos.json'
+        _productos = Documento.read(route)
+
+        ##
+        _id = data['id']
+
+        ##Traemos la data del producto en especifico
+        for Producto in _productos:
+            ##Buscar el objeto
+            if Producto['id'] == _id:
+                return Producto
+
+
+DAOProducto.drop()
         
--- handdle/document.py
+++ handdle/document.py
@@ -19,34 +19,35 @@ class Documento:
                 return file.name
 
     ##Escribimos en el documento
-    def write(route, data):
+    def write(route, data, modified=False):
         try:
             ##
-            data = json.dumps(data.__dict__)
-            data = json.loads(data)
+            if modified == False:
+                data = json.dumps(data.__dict__)
+                data = json.loads(data)
+                ##Obtener la data del JSON del document [LECTURA]
+                file = open(route, 'r')
+                _rows = json.load(file)
+                file.close()
+                ##validar si la data esta vacia
+                if _rows: ##Data-Llena
+                    ##Validar si la data obtenida es un array o un objeto
+                    if isinstance(_rows, list): ###Array
+                        ##Pushear el nuevo objeto al [array]
+                        _rows.append(data)
+                    else: ###Object
+                        _collection = []
+                            
+                        ##Pusheamos la data extraida
+                        _collection.append(_rows)
+                        ##Pusheamos la nueva data
+                        _collection.append(data)
 
-            ##Obtener la data del JSON del document [LECTURA]
-            file = open(route, 'r')
-            _rows = json.load(file)
-            file.close()
-
-            ##validar si la data esta vacia
-            if _rows: ##Data-Llena
-                ##Validar si la data obtenida es un array o un objeto
-                if isinstance(_rows, list): ###Array
-                    print('es un array')
-                    ##Pushear el nuevo objeto al [array]
-                    _rows.append(data)
-                else: ###Object
-                    _collection = []
-                        
-                    ##Pusheamos la data extraida
-                    _collection.append(_rows)
-                    ##Pusheamos la nueva data
-                    _collection.append(data)
-
-                    ##Setteamos
-                    _rows = _collection
+                        ##Setteamos
+                        _rows = _collection
+                else: ##Data-Vacia
+                    ##Setteamos el objeto (NUEVO) al JSONFile
+                    _rows = data
             else: ##Data-Vacia
                 ##Setteamos el objeto (NUEVO) al JSONFile
                 _rows = data
@@ -54,6 +55,20 @@ class Documento:
             ##Escribir / Sobrescribir la data en el archivo estatico
             file = open(route, 'w')
             file.write(json.dumps(_rows, indent=4))
-            file.close()   
+            file.close()
+
+            ##
+            return True 
         except:
             print('Ah ocurrido un error al intentar escribir en documento')
+
+    def read(route):
+        try:
+            ##
+            file = open(route, 'r')
+            _rows = json.load(file)
+            file.close()
+
+            return _rows
+        except:
+            print('Ah ocurrido un error al intentar leer en documento')
